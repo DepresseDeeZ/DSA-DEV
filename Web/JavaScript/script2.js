70d8e62a77bb17a7b6562366c599df16f67b3997
Umang Raval <umangraval0365@gmail.com>
2024-08-16 22:17:17 +0530

    day 6 Web Notes

---
+++ Web/JavaScript/script2.js
@@ -0,0 +1,29 @@
+console.log("Welcome to Variables");
+
+// var Variablesname = 18;
+// console.log(Variablesname);
+
+//let
+// let varName = "hello Wolrd!";
+// varName  = 19;
+// console.log(varName);
+
+//const
+// const varName = "hello Wolrd!";
+// varName  = 19;
+// console.log(varName);
+
+
+//variable with same name 
+// const varName = "hello Wolrd!";
+// const varName = "hello Wolrd!";
+
+
+//lexical bound variable 
+// let var = "umang";
+// console.log(var);
+
+//variables
+var fname = "Umang";
+let mname = "Arvindbhai";
+const lname = "Raval";
