ef8adbffcc783c6698a6e19395681a4e51c41b43
Umang Raval <umangraval0365@gmail.com>
2024-08-17 22:57:33 +0530

    day 7 operator in javascrip

---
+++ Web/JavaScript/script3.js
@@ -0,0 +1,140 @@
+//operators
+console.log("welcome to operators");
+
+// //arithmetic operators
+
+// const a =10;
+// const b =20;
+// let result  =0;
+// console.log("a",a);
+// console.log("b",b);
+
+// //addition
+// result=a+b;
+// console.log("addition is :",result);
+
+// //subtarction
+// result=a-b;
+// console.log("Subtraction is :",result);
+
+// //multiplicattion
+// result=a*b;
+// console.log("Multiplication is :",result);
+
+// //division
+// result=a/b;
+// console.log("division is :",result);
+
+// //exponent
+
+// result= b**a;
+// console.log("exponenet is : ",result);
+
+// //Modulo
+// result=b%a;
+// console.log("Modulo is :",result);
+
+// //increment  and decrement
+// result++;
+// console.log("increment",result);
+// result--;
+// console.log("decrement",result);
+
+
+// //Comparison Operator
+// // compariosn operators => true/false
+
+// const a =10;
+// const b =10;
+// console.log("a",a);
+// console.log("b",b);
+
+// console.log("a is greater than b? ", a>b);
+// console.log("b is greater than a? ", a<b);
+
+// //greater than equal to 
+// console.log("a is greater than equal  b? ", a>=b);
+
+// console.log("b is greater than equal a? ", a<=b);
+
+// //equality operators
+// //isqual 
+// console.log("is equal", a==b);
+
+// //no equal to 
+// console.log("is equal to ",a!=b)
+
+
+// //strict equality
+// console.log("strict equality",a===b);
+// //strict inequality
+// console.log("strict inequality",a!==b);
+
+
+// //strict  vs loose equality
+
+// //=== strict equality
+// //== loose equality
+// console.log("comapraing string with integer with loose eqaulity" ,5=="5");
+
+// console.log("comapraing string with integer with strict eqaulity" ,5==="5");
+
+// //Strict Equality
+// --> comparevlaues and datatypes
+// --> return true only if both are same 
+
+// //loose equality
+// -->doesnt compare data types
+
+// // the  good ones : ===  ,!==
+// // then bad ones : ==   ,!=
+
+// //example
+// console.log(''=='0');//false
+// console.log(''==0);//true
+// console.log(0=='0');//true
+
+// console.log(false=='false');//false
+// console.log(false==0);//true
+
+// console.log("5"==5)//true                                        
+
+
+//Logical operators
+// --> or  =  ||
+// --> and  =  &&
+// --> not  =  !
+
+// //AND
+// console.log(true && false); //false
+// console.log(true && false && true); //false
+
+// console.log(true && true); //true
+// console.log(true && true && true); //true
+
+
+// //OR
+// console.log(true || true || true); //true
+// console.log(true || false || true); //true
+// console.log(false || false || false); //false
+
+
+// console.log(true || false); //true
+// console.log(true || true); //true
+// console.log(false || false); //false
+
+
+//Assignment Operators
+// +=,-=,*=,/=
+
+// let st =  "hello";
+// st+="world";
+// console.log(st)
+
+
+
+
+
+
+
+
