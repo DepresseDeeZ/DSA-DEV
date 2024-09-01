d66e3db86464157ef8745048bcac526fec3f02be
Umang Raval <umangraval0365@gmail.com>
2024-09-01 22:03:51 +0530

    daily javascript

---
+++ Web/JavaScript/script17.js
@@ -0,0 +1,7 @@
+// import {dogs,numberOfDogs} from "./dogs.js";
+
+// console.log(`My dogs are ${dogs}`);
+// console.log(`I have ${numberOfDogs} dogs`);
+
+import a from "./test.js";
+console.log(a);
